fcb3ed85106be6fcf72fabd0e97def7d492e4c9b
Ryabov Artem <Ryabcev1990.22@gmail.com>
2018-04-07 21:04:45 +0300

    third commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<title>Document</title>
 </head>
 <body>
-	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, culpa assumenda porro voluptas deleniti aliquam itaque debitis fugiat ipsam officia commodi non, optio autem deserunt corporis nihil illum. Veritatis, molestias.</p>
 	<?php
 	/*$a = mysql_connect(SELECT * FROM articles);
 echo "$a['titles','text','views'];"
