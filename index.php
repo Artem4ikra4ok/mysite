349e2763c8149c4652270bcca754b49aa6a8d760
Ryabov Artem <Ryabcev1990.22@gmail.com>
2018-04-07 21:03:33 +0300

    second commit

--- index.php
+++ index.php
@@ -6,17 +6,7 @@
 	<title>Document</title>
 </head>
 <body>
-	<div id="dav">
-		<ul>
-			<li><a href="https://www.facebook.com/profile.php?id=100018172511451">facebook</li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
-	</div>
-
-	<p>привет артем</p>
+	
 	<?php
 	/*$a = mysql_connect(SELECT * FROM articles);
 echo "$a['titles','text','views'];"
@@ -29,36 +19,7 @@ elseif ($a == $b) {echo "a равен b";}
 else {echo "a меньше, чем b";}
 */?>
 
-<?php 
-	class User {
-		public $name = "Имя";
-		public $password = "пароль";
-		public $email = "емейл";
-		public $city = "город";
-
-		public function Hello() {
-			echo "Hello {$this->name}";
-		}
-	}
-$admin = new User();
-$admin->Hello();
-
-//$admin->name="Alexey";
-//$user1->name="Andrey";
-
-//echo $admin->name;
-//echo $user1->name;
- ?>
-	<div class="divchik">
-		<ul>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-		</ul>
 
 
-	</div>
 </body>
 </html>
